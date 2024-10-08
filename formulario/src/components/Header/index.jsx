23e6f655c691191ed997ec47447a9a1d4af52ee0
Gabriel Lima <gabrilima2004@gmail.com>
2024-10-08 10:35:55 -0300

    estruturação dos componentes

---
+++ formulario/src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import "./styleh.css"
+
+export default function Header (){
+    return (
+        <>
+        <header>
+            <div className="div-header">
+                <h1>Monitoria React</h1>
+                <p>Contato</p>
+            </div>
+        </header>
+        </>
+    )
+}
---
+++ formulario/src/components/Login/index.jsx
@@ -0,0 +1,16 @@
+import "./styled.css"
+
+export default function Login(){
+    return (
+        <>
+        <div className="div-principal">
+            <h2>Login</h2>
+            <div className="div-inputs-botao">
+                <input type="text" name="usuario" id="usuario" placeholder="Usuário" />
+                <input type="password" name="senha" id="senha" placeholder="Senha" />
+                <button type="submit" >Enviar</button>
+            </div>
+        </div>
+        </>
+    )
+}
